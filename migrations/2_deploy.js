8f65402a5821f5023326db5f4f52042f3a0757a6
pharmcoder <ekwealorobinna112@gmail.com>
2021-05-26 15:15:02 +0100

    initial commit

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,14 +1,15 @@
 const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
 
-module.exports = async function(deployer) {
-	//deploy Token
+module.exports = async function (deployer) {
+  //deploy Token
+  await deployer.deploy(Token);
 
-	//assign token into variable to get it's address
-	
-	//pass token address for dBank contract(for future minting)
+  //assign token into variable to get it's address
 
-	//assign dBank contract into variable to get it's address
+  //pass token address for dBank contract(for future minting)
 
-	//change token's owner/minter from deployer to dBank
+  //assign dBank contract into variable to get it's address
+
+  //change token's owner/minter from deployer to dBank
 };
